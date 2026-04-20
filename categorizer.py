d83b98161f5a63abb729c2d43aaef7e45e4c1f2a
typingspeed40wps <165609200+typingspeed40wps@users.noreply.github.com>
2026-04-20 16:24:32 +0530

    Update categorizer.py

--- categorizer.py
+++ categorizer.py
@@ -1,1 +1,13 @@
+def get_category(file):
+    ext = file.lower().split('.')[-1]
 
+    if ext in ['jpg','jpeg','png','gif']:
+        return 'Images'
+    elif ext in ['pdf','doc','docx','txt','ppt','pptx']:
+        return 'Documents'
+    elif ext in ['py','js','java','cpp','c','html','css']:
+        return 'Code'
+    elif ext in ['zip','rar','7z','tar']:
+        return 'Archives'
+    else:
+        return 'Others'
